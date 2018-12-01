c829b575db2e26f1c7f93415912b470848903320
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-11-30 21:35:23 -0500

    added test data

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { createStore } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux'; 
 
-const store = creteStore(rootReducer);
+const store = createStore(rootReducer);
 
-ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
