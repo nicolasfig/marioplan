95404f0689a8a7144e57fe46694076625f984981
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-12-01 17:07:00 -0500

    created authactions

---
+++ src/store/actions/authActions.js
@@ -0,0 +1,10 @@
+export const signIn = (credentials) => {
+    return ( dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+
+        firebase.auth().signInWithEmailAndPassword(
+            credentials.email,
+            credentials.password
+        );
+    }
+}
