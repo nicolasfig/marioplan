184b2df2ac30be4f27c516e90219b36cbdd6d3ee
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-11-30 22:10:36 -0500

    added actions

---
+++ src/config/fbConfig.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBd433rssNxG-jtC7koNRxiTf38WopChhg",
+    authDomain: "nicolasfig-marioplan.firebaseapp.com",
+    databaseURL: "https://nicolasfig-marioplan.firebaseio.com",
+    projectId: "nicolasfig-marioplan",
+    storageBucket: "nicolasfig-marioplan.appspot.com",
+    messagingSenderId: "910559902687"
+  };
+  firebase.initializeApp(config);
---
+++ src/store/actions/projectActions.js
@@ -0,0 +1,10 @@
+export const createProject = (project) => {
+    return (dispatch, getState) => {
+        // Make async call to database
+
+        dispatch({
+            type: 'CREATE_PROJECT',
+            project
+        })
+    }
+}
