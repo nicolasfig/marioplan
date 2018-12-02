6d191917dac30ac1f1016e05da5ec09aca6cb391
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-12-02 15:53:53 -0500

    deployed the app to firebase

---
+++ dist/precache-manifest.6f0f745f0e55765109505ed636bb0dcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23eac2a86be25ff8228e",
+    "url": "/static/js/main.23eac2a8.chunk.js"
+  },
+  {
+    "revision": "f883ae7a276c807408c2",
+    "url": "/static/js/1.f883ae7a.chunk.js"
+  },
+  {
+    "revision": "23eac2a86be25ff8228e",
+    "url": "/static/css/main.7ce6fed5.chunk.css"
+  },
+  {
+    "revision": "732788fa0a5b079c2ec345e049910ebb",
+    "url": "/index.html"
+  }
+];
