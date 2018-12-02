0bf07aac1ce5e7a36d3cd49011e8dffd80705a93
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-12-02 15:38:45 -0500

    Notifications component

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,6 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello, World");
 });
 
-
 const createNotification = (notification => {
     return admin.firestore().collection('notifications')
     .add(notification)
@@ -26,4 +25,19 @@ exports.projectCreated = functions.firestore
         }
 
         return createNotification(notification)
+});
+
+exports.userJoined = functions.auth.user()
+    .onCreate(user => {
+        return admin.firestore().collection('users')
+        .doc(user.uid).get().then(doc => {
+            const newUser = doc.data()
+            const notification = {
+                content: 'Joined the party',
+                user: `${newUser.firstName} ${newUser.lastName}`,
+                time: admin.firestore.FieldValue.serverTimestamp()
+            }
+
+            return createNotification(notification)
+        })
 });
