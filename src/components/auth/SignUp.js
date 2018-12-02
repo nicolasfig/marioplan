eead9297779c923433bd112ecc56c0a51e6bf3ea
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-12-01 20:26:53 -0500

    signup still missing verification

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { signUp } from '../../store/actions/authActions';
 
 class SignUp extends Component {
 
@@ -19,7 +20,7 @@ class SignUp extends Component {
 
     handleSubmit = (e) => {
        e.preventDefault();
-       console.log(this.state)
+       this.props.signUp(this.state)
     }
 
     render() {
@@ -61,4 +62,10 @@ const mapStateToProps = (state) => {
     }
 } 
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signUp: (newUser) => dispatch(signUp(newUser))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
